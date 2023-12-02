075f0119b473ac3f377c39a8c6febcfb220fdd81
Andreas Muehlmann <andi.muehlmann@arcor.de>
2023-12-02 20:29:32 +0100

    solved day1 part2

--- day-1/part2.hs
+++ day-1/part2.hs
@@ -3,24 +3,25 @@ import qualified Data.Text.IO as TIO
 import Data.Char
 import System.IO
 
+
 firstDigit :: T.Text -> Char
 firstDigit line = T.head firstNotDigitsDropped
     where firstNotDigitsDropped = T.dropWhile (not . isDigit) line
 
-textDigitsToDigits :: T.Text -> T.Text
-textDigitsToDigits = T.replace (T.pack "one") (T.pack "1")
-                     . T.replace (T.pack "two") (T.pack "2")
-                     . T.replace (T.pack "three") (T.pack "3")
-                     . T.replace (T.pack "four") (T.pack "4")
-                     . T.replace (T.pack "five") (T.pack "5")
-                     . T.replace (T.pack "six") (T.pack "6")
-                     . T.replace (T.pack "seven") (T.pack "7")
-                     . T.replace (T.pack "eight") (T.pack "8")
-                     . T.replace (T.pack "nine") (T.pack "9")
-
 lastDigit :: T.Text -> Char
 lastDigit line = T.last lastNotDigitsDropped
     where lastNotDigitsDropped = T.dropWhileEnd (not . isDigit) line
+
+textDigitsToDigits :: T.Text -> T.Text
+textDigitsToDigits = T.replace (T.pack "one") (T.pack "one1one")
+                     . T.replace (T.pack "two") (T.pack "two2two")
+                     . T.replace (T.pack "three") (T.pack "three3three")
+                     . T.replace (T.pack "four") (T.pack "four4four")
+                     . T.replace (T.pack "five") (T.pack "five5five")
+                     . T.replace (T.pack "six") (T.pack "six6six")
+                     . T.replace (T.pack "seven") (T.pack "seven7seven")
+                     . T.replace (T.pack "eight") (T.pack "eight8eight")
+                     . T.replace (T.pack "nine") (T.pack "nine9nine")
 
 lastFirstDigitAsNumber :: T.Text -> Integer
 lastFirstDigitAsNumber line = read concatedDigits :: Integer
