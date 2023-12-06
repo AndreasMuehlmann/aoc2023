4a5a494b7be0a27188ff669e30c65a95fd57ae5f
Andreas Mühlmann <andreas.muehlmann@rohde-schwarz.com>
2023-12-06 10:20:01 +0100

    parsing digits

--- day-3/part1/main.hs
+++ day-3/part1/main.hs
@@ -1,8 +1,25 @@
-import qualified Data.Text as T
-import qualified Data.Text.IO as TIO
+module Main where
 
+import Data.Char
+
+
+data DigitCell = DigitCellCons Int Int Int 
+data CharCell = CharCellCons Char Int Int 
+
+instance Show DigitCell where
+   show (DigitCellCons digit x y) = show [digit, x, y]
+
+solve :: [DigitCell] -> [CharCell] -> Int
+solve cells text = 0
+
+parseDigits :: String -> [DigitCell] -> Int -> Int-> [DigitCell]
+parseDigits (char:chars) cellsAccumulator x y 
+        | null chars = if isDigit char then DigitCellCons (digitToInt char) x y : cellsAccumulator else cellsAccumulator
+        | isDigit char = parseDigits chars (DigitCellCons (digitToInt char) x y : cellsAccumulator) (x + 1) y
+        | char == '\n' = parseDigits chars cellsAccumulator 0 (y + 1)
+        | otherwise = parseDigits chars cellsAccumulator (x + 1) y
 
 main :: IO ()
 main = do
-    contents <- TIO.readFile "test_part1.txt"
-    print contents
+    contents <- readFile "test_part1.txt"
+    print $ solve (parseDigits contents [] 0 0) []
