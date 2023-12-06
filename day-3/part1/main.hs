0b61d439e40d0a5cab1556e1a7ad3ee744afdf54
Andreas Muehlmann <andi.muehlmann@arcor.de>
2023-12-06 21:51:56 +0100

    getNumber other solve

--- day-3/part1/main.hs
+++ day-3/part1/main.hs
@@ -2,24 +2,25 @@ module Main where
 
 import Data.Char
 
+getNumberHelper :: [String] -> Int -> Int -> Int -> String
+getNumberHelper stringLines x y direction 
+                | isDigit char && direction < 0 = getNumberHelper stringLines (x + direction) y direction ++ [char]
+                | isDigit char && direction > 0 = char : getNumberHelper stringLines (x + direction) y direction
+                | otherwise = []
+                where char = (stringLines!!y)!!x 
+ 
 
-data DigitCell = DigitCellCons Int Int Int 
-data CharCell = CharCellCons Char Int Int 
+getNumber :: [String] -> Int -> Int -> String
+getNumber stringLines x y = getNumberHelper stringLines (x - 1) y (-1) ++ getNumberHelper stringLines x y 1
 
-instance Show DigitCell where
-   show (DigitCellCons digit x y) = show [digit, x, y]
-
-solve :: [DigitCell] -> [CharCell] -> Int
-solve cells text = 0
-
-parseDigits :: String -> [DigitCell] -> Int -> Int-> [DigitCell]
-parseDigits (char:chars) cellsAccumulator x y 
-        | null chars = if isDigit char then DigitCellCons (digitToInt char) x y : cellsAccumulator else cellsAccumulator
-        | isDigit char = parseDigits chars (DigitCellCons (digitToInt char) x y : cellsAccumulator) (x + 1) y
-        | char == '\n' = parseDigits chars cellsAccumulator 0 (y + 1)
-        | otherwise = parseDigits chars cellsAccumulator (x + 1) y
-
+solve :: String -> Int -> Int -> Int -> Int
+solve (char:chars) x y accumulator
+                | isDigit char || (char == '.') = solve chars (x + 1) y accumulator
+                | char == '\n' = solve chars 0 (y + 1) accumulator
+                | null chars = accumulator -- sum . map getNumber . map position + rel_positions
+                | otherwise = 0 --solve  chars (x + 1) y (accumulator + (sum . map getNumber . map position + rel_positions))
+                where rel_positions = [(relative_x, relative_y) | relative_x <- [-1, 0, 1], relative_y <- [-1, 0, 1], relative_y /= 0 && relative_x /= 0]
 main :: IO ()
 main = do
     contents <- readFile "test_part1.txt"
-    print $ solve (parseDigits contents [] 0 0) []
+    print $ solve contents 0 0 0
