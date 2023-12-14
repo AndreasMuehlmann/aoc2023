cf86335f125c1b97acbf6e29b183ab58f3e09158
Andreas Mühlmann <andreas.muehlmann@rohde-schwarz.com>
2023-12-14 16:05:42 +0100

    day 8 part 2 takes too long

--- day-8/part2/main.hs
+++ day-8/part2/main.hs
@@ -4,69 +4,48 @@ import qualified Data.Text as T
 import qualified Data.Text.Read as TR
 import qualified Data.Text.IO as TIO
 
-import Data.List ( sortBy, group, sort)
-import Data.Char ( digitToInt, isDigit )
+import Data.Maybe
+import Data.List
 
+data Node = Node {name :: String, edges :: [Int]} deriving Show
 
-textToInt :: T.Text -> Int
-textToInt text = case TR.decimal text of
-    Left _err -> -1
-    Right (result, _) -> result
+onlySpaces :: T.Text -> T.Text
+onlySpaces = T.replace (T.pack "=") (T.pack " ") . T.replace (T.pack "(") (T.pack " ")
+             . T.replace (T.pack ")") (T.pack " ") . T.replace (T.pack ",") (T.pack " ")
 
-parseCardSet :: T.Text -> (T.Text, Int)        
-parseCardSet line = (head wordsLine, textToInt $ last wordsLine)
-             where wordsLine = T.words line
-    
-parseCardSets :: T.Text -> [(T.Text, Int)]
-parseCardSets = map parseCardSet . T.lines
+parseNodeAsTuple :: T.Text -> (String, [String])
+parseNodeAsTuple line = (head nameAndEdges, tail nameAndEdges)
+        where nameAndEdges = words $ T.unpack $ onlySpaces line
 
-valueCard :: Char -> Int
-valueCard card
-        | isDigit card = digitToInt card
-        | card == 'T' = 10
-        | card == 'J' = 1
-        | card == 'Q' = 11
-        | card == 'K' = 12
-        | card == 'A' = 13
+toIndex :: [String] -> String -> Int
+toIndex nodeNames edge = fromJust maybeIndex
+        where maybeIndex = elemIndex edge nodeNames
 
-totalValueCards :: String -> Int
-totalValueCards [card] = valueCard card
-totalValueCards (card:cards) = valueCard card * (14 ^ length (card:cards)) + totalValueCards cards
+toNodes :: Int -> [Node] -> [(String, [String])]-> [Node]
+toNodes index nodes tupleNodes
+        | index >= length tupleNodes = nodes
+        | otherwise = toNodes (index + 1) (nodes ++ [Node (fst $ tupleNodes!!index) $ map (toIndex nodeNames) (snd $ tupleNodes!!index)]) tupleNodes
+        where nodeNames = map fst tupleNodes
 
-typeCardSet :: String -> Int
-typeCardSet cards
-        | jokers == 5 = 6
-        | length groupedCards == 1 = 6
-        | length groupedCards == 2 && maximum lengthGroupedCards == 4 - jokers = 5
-        | length groupedCards == 2 = 4
-        | length groupedCards == 3 && maximum lengthGroupedCards == 3 - jokers = 3
-        | length groupedCards == 3 = 2
-        | length groupedCards == 4 = 1
-        | otherwise = 0
-        where jokers = length $ filter (== 'J') cards
-              filteredCards = filter (/= 'J') cards
-              groupedCards = group $ sort filteredCards
-              lengthGroupedCards = map length groupedCards
-              
+parseMap :: T.Text -> (String, [Node])
+parseMap text = (T.unpack $ head linesText, toNodes 0 [] $ map parseNodeAsTuple $ tail $ tail linesText)
+        where linesText = T.lines text
 
-isFirstStronger :: T.Text -> T.Text -> Bool
-isFirstStronger cards1 cards2
-        | typeCards1 > typeCards2 = True
-        | typeCards1 < typeCards2 = False
-        | otherwise = totalValueCards (T.unpack cards1) > totalValueCards (T.unpack cards2)
-        where typeCards1 = typeCardSet $ T.unpack cards1
-              typeCards2 = typeCardSet $ T.unpack cards2
+doInstruction :: Bool -> [Node] -> Node -> Node
+doInstruction goLeft nodes node
+        | goLeft = nodes!!head (edges node)
+        | otherwise = nodes!!last (edges node)
 
-compareCardSets :: (T.Text, Int) -> (T.Text, Int) -> Ordering
-compareCardSets (cards1, bid1) (cards2, bid2)
-        | cards1 == cards2 = EQ
-        | isFirstStronger cards1 cards2 = GT
-        | otherwise = LT
+solveHelper :: String -> [Node] -> [Node] -> Int -> Int
+solveHelper instructions nodes currentNodes steps
+        | all (\node -> last (name node) == 'Z') currentNodes = steps
+        | otherwise = solveHelper instructions nodes (map (doInstruction (currentInstruction == 'L') nodes) currentNodes) (steps + 1)
+        where currentInstruction = instructions!!(steps `mod` length instructions)
 
-solve :: [(T.Text, Int)] -> Int
-solve cardSets = sum $ zipWith (*) [1..] $ map snd $ sortBy compareCardSets cardSets
+solve :: (String, [Node]) -> Int
+solve (instructions, nodes) = solveHelper instructions nodes (filter (\node -> last (name node) == 'A') nodes) 0
 
 main :: IO ()
 main = do
     contents <- TIO.readFile "part2.txt"
-    print $ solve $ parseCardSets contents
+    print $ solve $ parseMap contents
